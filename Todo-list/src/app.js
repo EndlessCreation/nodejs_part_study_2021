8fbea87425bf227333e1aa02a7c5d3c7d731a20f
dnatuna <dnatuna123@gmail.com>
2021-03-27 21:14:08 +0900

    Fix: CORS header setting

--- Todo-list/src/app.js
+++ Todo-list/src/app.js
@@ -30,7 +30,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, PATCH, POST, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization'
+    'Origin',
+    'Accept',
+    'X-Requested-With',
+    'Content-Type',
+    'Access-Control-Request-Method',
+    'Access-Control-Request-Headers',
+    'Authorization'
   );
   next();
 });
